6bea25e40cd28ffd6f1907323168c82dd7838f08
PauloDiniz05 <paulodiniz1105@gmail.com>
2025-05-22 22:21:00 -0300

    Virtual phone parte 2

---
+++ virtual-phone/main.js
@@ -0,0 +1,25 @@
+const display =
+    document.querySelector("input[type=tel]");
+
+const teclas =
+    document.querySelectorAll("input[type=button]");
+
+const limpar = 
+        document.querySelector(".icon");
+
+limpar.onclick = () => {
+        const opcao = confirm("Deseja mesmo continuar?")
+        if(opcao)
+        display.value = "";
+    }
+
+//criar condição para travar mais de 14 digitos
+
+function valorTeclaToDisplay (i) {
+    display.value += teclas[i].value;
+}
+
+for(let i = 0; i < teclas.length; i++)
+teclas[i].onclick = () => {
+    valorTeclaToDisplay(i);
+}
